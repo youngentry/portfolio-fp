baa809e602e96874a2be728a04cf606f1b2e1671
youngentry <gentry_@naver.com>
2022-10-26 10:14:55 +0900

    221026 스마트폰mockup 이미지 추가, github링크버튼 추가

--- src/Main.js
+++ src/Main.js
@@ -2,6 +2,7 @@ import React, { useEffect, useRef, useState } from "react";
 import ReactFullpage from "@fullpage/react-fullpage";
 import "./Main.scss";
 import { profile, portfolio } from "./Data";
+import { FaGithub } from "react-icons/fa";
 
 //portfolio배열에서 title을 빼서 배열을 만들어 쓰겠다.
 const AC = portfolio.map((it) => (it = it.title));
@@ -62,77 +63,87 @@ const Main = () => {
                 render={({ state, fullpageApi }) => {
                     return (
                         <ReactFullpage.Wrapper>
-                            <div className="section main">
-                                <div className="case">
-                                    <h1>FRONTEND WEB DEVELOPER</h1>
-                                    <p>
-                                        <span>
-                                            원리에 관심이 많은 예비 프론트엔드 개발자 <strong>{profile.name}</strong>입니다
-                                        </span>
-                                    </p>
+                            <div className="Wrapper">
+                                <div className="section main">
+                                    <div className="case">
+                                        <h1>FRONTEND WEB DEVELOPER</h1>
+                                        <p>
+                                            <span>
+                                                원리에 관심이 많은 예비 프론트엔드 개발자 <strong>{profile.name}</strong>입니다
+                                            </span>
+                                        </p>
+                                    </div>
                                 </div>
-                            </div>
 
-                            {portfolio.map((it, idx) => {
-                                return (
-                                    <div className="section">
-                                        <div className="case">
-                                            <div className="inner">
-                                                <div className="images">
-                                                    <a href={it.link} target="_blank">
-                                                        <div className="mock">
-                                                            <figure>
-                                                                <img src={it.mock} alt={it.title} />
-                                                            </figure>
-                                                            <div className="screen"></div>
-                                                        </div>
-                                                        <div className="cell">
-                                                            <img src={it.cell} alt={it.title} />
-                                                        </div>
-                                                    </a>
+                                {portfolio.map((it, idx) => {
+                                    return (
+                                        <div className="section" key={it.id}>
+                                            <div className="case">
+                                                <div className="inner">
+                                                    <div className="images">
+                                                        <a href={it.link} target="_blank">
+                                                            <div className="mock">
+                                                                <figure>
+                                                                    <img src={it.mock} alt={it.title} />
+                                                                </figure>
+                                                                <div className="screen"></div>
+                                                            </div>
+                                                            <div className="cell">
+                                                                <figure>
+                                                                    <img src={it.cell} alt={it.title} />
+                                                                </figure>
+                                                                <div className="cellScreen"></div>
+                                                            </div>
+                                                        </a>
+                                                    </div>
+                                                    <ul className="des">
+                                                        <li>
+                                                            <h2>{it.title}</h2>
+                                                        </li>
+                                                        <li className="type">
+                                                            <span>TYPE</span>
+                                                            <strong> {it.type}</strong>
+                                                        </li>
+                                                        <li>
+                                                            <span>FONT</span>
+                                                            {it.font.map((el) => {
+                                                                return <strong className="skill">{el} </strong>;
+                                                            })}
+                                                        </li>
+
+                                                        <li className="lang">
+                                                            <span>SKILL</span>
+                                                            {it.skill.map((el) => {
+                                                                return <strong className="skill">{el} </strong>;
+                                                            })}
+                                                        </li>
+                                                        <li>
+                                                            <span>DATE</span>
+                                                            <strong> {it.date}</strong>
+                                                        </li>
+
+                                                        <li className="link">
+                                                            <div>
+                                                                <a href={it.link} target="_blank" rel="noreferrer">
+                                                                    <button>VEIW ON WEB</button>
+                                                                </a>
+                                                            </div>
+                                                            <div>
+                                                                <a href={it.gitgub_link} target="_blank" rel="noreferrer">
+                                                                    <button>GitHub</button>
+                                                                </a>
+                                                            </div>
+                                                        </li>
+                                                        <li>
+                                                            <p>{it.info}</p>
+                                                        </li>
+                                                    </ul>
                                                 </div>
-                                                <ul className="des">
-                                                    <li>
-                                                        <h2>{it.title}</h2>
-                                                    </li>
-                                                    <li className="type">
-                                                        <span>TYPE</span>
-                                                        <strong> {it.type}</strong>
-                                                    </li>
-                                                    <li>
-                                                        <span>FONT</span>
-                                                        {it.font.map((el) => {
-                                                            return <strong className="skill">{el} </strong>;
-                                                        })}
-                                                    </li>
-
-                                                    <li>
-                                                        <span>SKILL</span>
-                                                        {it.skill.map((el) => {
-                                                            return <strong className="skill">{el} </strong>;
-                                                        })}
-                                                    </li>
-                                                    <li>
-                                                        <span>DATE</span>
-                                                        <strong> {it.date}</strong>
-                                                    </li>
-
-                                                    <li className="link">
-                                                        <div>
-                                                            <a href={it.link} target="_blank" rel="noreferrer">
-                                                                <button>VEIW ON WEB</button>
-                                                            </a>
-                                                        </div>
-                                                    </li>
-                                                    <li>
-                                                        <p>{it.info}</p>
-                                                    </li>
-                                                </ul>
                                             </div>
                                         </div>
-                                    </div>
-                                );
-                            })}
+                                    );
+                                })}
+                            </div>
                         </ReactFullpage.Wrapper>
                     );
                 }}
